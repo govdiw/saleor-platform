44fb5f91db0b6a0d32a092392c4c4ad7a141ef36
web.admin <web.admin@sourcegoodfood.com>
2021-08-28 21:07:35 +0530

    traefik updates

---
+++ build-saleor-clean.sh
@@ -0,0 +1,4 @@
+docker-compose build --no-cache
+docker-compose run --rm api python3 manage.py migrate
+docker-compose run --rm api python3 manage.py collectstatic --noinput
+docker-compose run --rm api python3 manage.py populatedb
