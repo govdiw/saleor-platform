f1d724b0d14d667951b6341ce848db772d49864a
web.admin <web.admin@sourcegoodfood.com>
2021-08-29 11:44:29 +0530

    traefik

--- build-saleor-clean.sh
+++ build-saleor-clean.sh
@@ -1,4 +1,5 @@
-docker-compose build --no-cache
+docker-compose -f docker-final.yml build --no-cache
 docker-compose run --rm api python3 manage.py migrate
 docker-compose run --rm api python3 manage.py collectstatic --noinput
 docker-compose run --rm api python3 manage.py populatedb
+docker-compose -f docker-final.yml up && docker-compose -f storefront.yml --no-cache
