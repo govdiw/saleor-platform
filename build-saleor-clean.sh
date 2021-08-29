dbb2ae354413508c4d7be0942802bf3a1f8fb46c
web.admin <web.admin@sourcegoodfood.com>
2021-08-29 22:04:09 +0530

    updated to fix version issues

--- build-saleor-clean.sh
+++ build-saleor-clean.sh
@@ -1,5 +1,12 @@
+docker-compose down
+docker rm -f $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
+docker rmi -f $(docker images -a | grep "saleor")
 docker-compose -f docker-final.yml build --no-cache
-docker-compose run --rm api python3 manage.py migrate
-docker-compose run --rm api python3 manage.py collectstatic --noinput
-docker-compose run --rm api python3 manage.py populatedb
-docker-compose -f docker-final.yml up && docker-compose -f storefront.yml --no-cache
+docker-compose -f docker-final.yml run --rm api python3 manage.py migrate
+docker-compose -f docker-final.yml run --rm api python3 manage.py collectstatic --noinput
+docker-compose -f docker-final.yml run --rm api python3 manage.py populatedb
+#docker-compose -f docker-final.yml up &
+echo ' waiting for saleor server to start'
+sleep(20)
+docker-compose -f storefront.yml --no-cache
